b2edf71903c84786dc3865c637cbb5263cb4f78b
davidz1234567890 <129808533+davidz1234567890@users.noreply.github.com>
2024-01-31 16:31:06 -0500

    Update eulerian_path.py

--- eulerian_path.py
+++ eulerian_path.py
@@ -8,6 +8,13 @@ def is_empty_list(g: Dict[int, List[int]]):
             return False
     return True
 
+def lowest_index(g: Dict[int, List[int]]):
+    index = 100000000
+    for i in g:
+        if i < index:
+            index = i
+    return index
+
 def index_not_empty(llist, g: Dict[int, List[int]]):
     for i in llist:
         if len(g[i]) > 0:
@@ -19,9 +26,10 @@ def eulerian_cycle(g: Dict[int, List[int]]) -> Iterable[int]:
     """Constructs an Eulerian cycle in a graph."""
     #print("hello")
     solution = []
-    solution.append(0)
-    index = 0
-    next = g[0][0]
+    solution.append(lowest_index(g))
+    index = lowest_index(g)
+    next = g[index][0]
+    
     #print("next = ")
     #print(next)
     count = 0
@@ -34,8 +42,9 @@ def eulerian_cycle(g: Dict[int, List[int]]) -> Iterable[int]:
         next = g[index][0]
     g[index].remove(next)
     index = index_not_empty(solution, g)
-    print("here is index in line 34")
-    print(index)
+    if(index < 0):
+        solution.append(lowest_index(g))
+        return solution
     #while solution[0] != index:
     #    m = solution[0]
     #    del solution[0]
@@ -43,43 +52,35 @@ def eulerian_cycle(g: Dict[int, List[int]]) -> Iterable[int]:
         
     #print(solution)
     next = g[index][0]
-    print("here is next:")
-    print(next)
+    
     #while is_empty_list(g)==False:
     temp = solution.index(index)
-    while count != 7:    
+    while is_empty_list(g)==False:    
         
-        print(temp)
+      
         solution.insert(temp+1, next)
         temp = temp + 1
         
         g[index].remove(next)
         index = next
-        print("reaching here")
+      
         if(len(g[next])>0):
             next = g[next][0]
         else:
             if(is_empty_list(g)==True):
-                print("here")
+                
                 break
             
             index = index_not_empty(solution, g)
             next = g[index][0]
             temp = solution.index(index)
-            print("index before break")
-            print(index)
-            print("next before break")
-            print(next)
+          
             continue
-        print("index out")
-        print(index)
-        print("next out")
-        print(next)
-        print(g)
+     
             
             
         
         count += 1
     #print("solution = ")
-    solution.append(0)
+    solution.append(lowest_index(g))
     return solution
